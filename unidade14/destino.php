16ad4fe0e164c209f16778f64fa31d498bb95125
José Alberto Rodrigues Neto <josealberto270920@gmail.com>
2024-09-25 23:28:13 -0300

    Section 77: Course IX - Unit 14 - Form - 345. Identify the ternary operator

--- unidade14/destino.php
+++ unidade14/destino.php
@@ -14,21 +14,27 @@
 
         <?php
             // isset => Pergunta se está configurada uma determinada variável
+            // ? = if
+            // : = else
 
-            if(isset($_POST["nome"])) {
-                $_nome = $_POST["nome"];
-            } else {
-                $_nome = "Sem definição";
-            }
+            // if(isset($_POST["nome"])) {
+            //     $_nome = $_POST["nome"];
+            // } else {
+            //     $_nome = "Sem definição";
+            // }
 
-            if(isset($_POST["email"])) {
-                $_email = $_POST["email"];
-            } else {
-                $_email = "Sem definição";
-            }
+            
+            // if(isset($_POST["email"])) {
+            //     $_email = $_POST["email"];
+            // } else {
+            //     $_email = "Sem definição";
+            // }
+            
+            $_nome = isset($_POST["nome"]) ? $_POST["nome"] : "Sem definição";
+            $_email = isset($_POST["email"]) ? $_POST["email"] : "Sem definicão";
 
             echo "Nome: " . $_nome . "<br>";
-            echo "Nome: " . $_email . "<br>";
+            echo "Email: " . $_email . "<br>";
             // echo "Nome: " . $_POST["nome"];
             // echo "Nome: " . $_POST["email"];
 
